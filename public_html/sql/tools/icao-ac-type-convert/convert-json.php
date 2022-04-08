bf9c47b02706a0e69b006b77e8b449fea31393fc
l4red0 <leszek.soltys@gmail.com>
2022-04-08 12:14:37 +0200

    Aircraft Type Designators update

---
+++ public_html/sql/tools/icao-ac-type-convert/convert-json.php
@@ -0,0 +1,26 @@
+<?php
+# This script converts raw JSON data from ICAO at https://www.icao.int/publications/DOC8643/ (Aircraft Type Designators) to format used in `/public_html/db/aircraft_types/icao_aircraft_types.json'
+
+$json = file_get_contents("./raw-icao-db.json"); #Raw json path to file
+$jsonOut = "./icao_aircraft_types.json"; #Output file
+
+$rawArray = json_decode($json, true);
+$outArray = [];
+
+foreach ($rawArray as $field => $value) {
+		 $outArray[$value['Designator']]['desc'] = $value['Description'];
+		 $outArray[$value['Designator']]['wtc'] = $value['WTC'];
+}
+
+$arrJson = json_encode($outArray);
+	$file = fopen($jsonOut,'w');
+	if(fwrite($file, $arrJson) === false){
+		print_r("<pre>".$jsonOut." > write failed.");
+	} else {
+		print_r("<pre>".$jsonOut." > saved successfully.</pre>");
+	}
+	fclose($file);
+
+#echo '<pre>' . print_r($arrJson, true) . '</pre>';
+
+ ?>
